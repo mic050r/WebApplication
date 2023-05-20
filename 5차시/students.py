eed3bedc5c4e2acdf4d25c348b2a4057bcd62e2c
mic050r <103114387+mic050r@users.noreply.github.com>
2023-05-21 03:21:40 +0900

    [5차시] [feat] : 데이터를 삽입하는 함수 작성

--- 5차시/students.py
+++ 5차시/students.py
@@ -16,3 +16,19 @@ def create_table():
 
 
 create_table()
+
+
+def insert_student(name, age, major):
+    connection = sqlite3.connect("students.db")
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """INSERT INTO students(name, age, major)
+    VALUSE (?,?,?)""",
+        (name, age, major),
+    )
+    connection.commit()
+    connection.close()
+
+
+insert_student("john", 21, "computer science")
