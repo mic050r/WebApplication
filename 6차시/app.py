6d8ccfa7e6c5f304fca4e0d9a240146f3120ce56
mic050r <103114387+mic050r@users.noreply.github.com>
2023-05-21 19:42:09 +0900

    [6차시] SQLite 데이터베이스 초기화 및 연동

--- 6차시/app.py
+++ 6차시/app.py
@@ -2,3 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
+
+
+def init_def():
+    connection = sqlite3.connect("example.db")
+    cursor = connection.cursor()
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS tasks
+    (id INTEGER PRIMARY KEY, task TEXT)"""
+    )
+    connection.commit()  # 저장
+    connection.close()  # 종료
